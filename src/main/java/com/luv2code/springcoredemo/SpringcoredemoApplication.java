a2564e40d46f938f574e5241df543ca18dd67231
juan.zea <juan.zea@innclod.com>
2023-09-14 23:19:20 -0500

    Setter injection

--- src/main/java/com/luv2code/springcoredemo/SpringcoredemoApplication.java
+++ src/main/java/com/luv2code/springcoredemo/SpringcoredemoApplication.java
@@ -3,22 +3,8 @@ package com.luv2code.springcoredemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/*
-* The Util package isn't inside the default package ('springcoredemo')
-* so we must define explicitly the package structure for
-* our Spring Application
-* */
-@SpringBootApplication(
-	// We have to declare this field to
-	// list our packages and make them
-	// available for the Spring project.
-	scanBasePackages = {
-		"com.luv2code.springcoredemo",
-		"com.luv2code.util"
-	}
-)
+@SpringBootApplication
 public class SpringcoredemoApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringcoredemoApplication.class, args);
 	}
--- src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
+++ src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
@@ -1,12 +1,12 @@
 package com.luv2code.springcoredemo.rest;
 
-import com.luv2code.util.Coach;
+import com.luv2code.springcoredemo.common.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /*
-* To use like a Rest Controller we need that annotation
+* To use this like a Rest Controller we need that annotation
 * */
 @RestController
 public class DemoController
@@ -14,18 +14,17 @@ public class DemoController
     // Define a private field for the dependency
     private Coach myCoach;
 
+    public DemoController()
+    {
+
+    }
+
     /*
-    * Define the constructor to make the
-    * dependency injection
-    *
-    * At this moment, we defined only one component
-    * to be injected that is CricketCoach, so we use
-    * the Coach interface and then the @Autowired will
-    * look for the Component that matches with the
-    * type Coach, in this case CricketCoach class.
+     * Thanks, the @Autowired annotation the dependency
+     * will be injected automatically when we run the project.
      * */
     @Autowired // -> This annotation tells spring to inject a dependency
-    public DemoController(Coach myCoach)
+    public void setCoach(Coach myCoach)
     {
         this.myCoach = myCoach;
     }
