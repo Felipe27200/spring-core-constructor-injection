c542cb29d54eba2a4b94d17ed304c8d8d81c23a9
juan.zea <juan.zea@innclod.com>
2023-09-16 21:19:58 -0500

    Make multiple heritance and choose one with qualifiers

--- src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
+++ src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
@@ -2,6 +2,7 @@ package com.luv2code.springcoredemo.rest;
 
 import com.luv2code.springcoredemo.common.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,17 +15,18 @@ public class DemoController
     // Define a private field for the dependency
     private Coach myCoach;
 
-    public DemoController()
-    {
-
-    }
-
     /*
      * Thanks, the @Autowired annotation the dependency
      * will be injected automatically when we run the project.
      * */
     @Autowired // -> This annotation tells spring to inject a dependency
-    public void setCoach(Coach myCoach)
+
+    /*
+    * Must use @Qualifier to indicate which bean(dependency)
+    * Spring has to use
+    * */
+    // public DemoController(@Qualifier("baseballCoach") Coach myCoach)
+    public DemoController(@Qualifier("trackCoach") Coach myCoach)
     {
         this.myCoach = myCoach;
     }
